1d4b906d41eb69a4d974b79a6b9034a9379255ac
ajarombek <ajarombek95@gmail.com>
2020-11-25 20:20:37 -0500

    quick fix to aws_test_modules package

--- aws_test_functions/README.md
+++ aws_test_functions/README.md
@@ -11,3 +11,7 @@ A Python package containing reusable functions for testing AWS infrastructure.  
 | `Pipfile`            | Used to install dependencies and create a virtual environment with `pip`.                    |
 | `requirements.txt`   | Used to install dependencies and create a virtual environment with `pipenv`.                 |
 | `setup.py`           | Build script to create a Python package.                                                     |
+
+### Resources
+
+1. [Packaging Python Projects](https://packaging.python.org/tutorials/packaging-projects/)
--- aws_test_functions/setup.py
+++ aws_test_functions/setup.py
@@ -10,15 +10,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='ajarombek-aws-test-functions',
-    version='0.2.0',
+    name='aws_test_functions',
+    version='0.2.2',
     author='Andrew Jarombek',
     author_email='andrew@jarombek.com',
     description='A Python package containing reusable functions for testing AWS infrastructure',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AJarombek/cloud-modules/aws-test-functions',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['aws_test_functions', 'aws_test_functions.*']),
     classifiers=[],
     python_requires='>=3.8'
 )
